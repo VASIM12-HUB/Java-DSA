918b70d273fd76c117653d35e69b3cc5930876a3
VASIM12-HUB <214G1A32B7@srit.ac.in>
2025-05-16 11:32:27 +0530

    Auto-update on 05/16/2025 11:32:27

---
+++ basic/f1.java
@@ -0,0 +1,26 @@
+public class f1 {
+    public static void main(String[] args){
+        int integer = 5;
+        float decimal = 5.99f;
+        char letter='D';
+        boolean flag=true;
+        String text = "Hello";
+        System.out.println("Hello World");
+        System.out.print("Next line");
+        System.out.print("Another line without new line");
+        System.out.println(3+3);
+        System.out.println(6*5);
+        System.out.println(integer);
+        System.out.println(decimal);
+        System.out.println(letter);
+        System.out.println(flag);
+        System.out.println(text);
+        //this is comment to test
+        /* multi line comment to test
+         * lets break this, but it is adding an extra start
+         * and end to the comment
+         */
+
+
+    }
+}

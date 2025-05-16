877de04223d41684b50996b8e846e5dbc8ba8cbe
VASIM12-HUB <214G1A32B7@srit.ac.in>
2025-05-16 19:54:53 +0530

    Auto-commit on %date% %time%

--- Arrays/Set Mat Zero/SetMatrixZeroBetter.java
+++ Arrays/Set Mat Zero/SetMatrixZeroBetter.java
@@ -1,37 +1,26 @@
 import java.util.*;
-public class SetMatrixZero{
-   static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i){
-        for(int j = 0;j < m; j++){
-            if(matrix.get(i).get(j) != 0){
-                matrix.get(i).set(j, -1);
-            }
-        }
-   } 
-   static void markCol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j){
-        for(int i = 0; i < n; i++ ){
-            if(matrix.get(i).get(j) != 0){
-                matrix.get(i).set(j, -1);
-            }
-        }
-    }
-    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n,int m){
-        for(int i = 0; i < n; i++){
+public class SetMatrixZeroBetter{
+   static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m){
+       int[] row = new int[n];
+       int[] col = new int[m];
+       for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(matrix.get(i).get(j) == 0){
-                    markRow(matrix, n, m, i);
-                    markCol(matrix, n, m, j);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
-        } 
-        for(int i = 0; i < n; i++){
+       }
+       for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(matrix.get(i).get(j)== -1){
+                if(row[i] == 1 || col[j] == 1){
                     matrix.get(i).set(j, 0);
                 }
             }
         }
         return matrix;
-    }
+   }
+   
     public static void main(String[] args){
         ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
         matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1, 0)));
